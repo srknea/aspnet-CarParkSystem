e20d5fd930f6946f24c39e04419310b3ebc0089e
Serkan IŞIK <dehnas79@gmail.com>
2023-09-21 13:14:43 +0300

    CustomResponse

---
+++ CarParkSystem.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,33 @@
+﻿using CarParkSystem.Core.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarParkSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction] // Aşağıdaki fonksiyon end-point değildir...
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            /* 204'ü geriye bir data dönmediğimiz sadece işlemin başarılı olduğunu client'a söylemek
+            istediğimiz durumlarda kullanırız. Örneğin bir kayıt ekleme işlemi yaptık ve geriye 
+            bir data dönmek istemiyoruz veya bir kayıt silme işlemi yaptık ve geriye bir data dönmek 
+            istemiyoruz. Bu durumlardada da 204 dönebiliriz. */
+
+            if (response.StatusCode == 204) 
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            } 
+                
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
