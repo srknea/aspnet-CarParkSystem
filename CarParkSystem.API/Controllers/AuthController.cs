471d0203389983b86d680a14f46cb007594d846c
Serkan IŞIK <dehnas79@gmail.com>
2023-09-22 15:00:23 +0300

    Fix error related with Swagger

--- CarParkSystem.API/Controllers/AuthController.cs
+++ CarParkSystem.API/Controllers/AuthController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CarParkSystem.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AuthController : CustomBaseController
     {
@@ -61,5 +61,4 @@ namespace CarParkSystem.API.Controllers
         }
     }
 
-}
 }
