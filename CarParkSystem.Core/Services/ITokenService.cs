dc6a7fd79d2134515ff54bdd04df89f3f6976772
Serkan IŞIK <dehnas79@gmail.com>
2023-09-22 09:34:55 +0300

    Add ITokenService

---
+++ CarParkSystem.Core/Services/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using CarParkSystem.Core.DTOs;
+using CarParkSystem.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParkSystem.Core.Services
+{
+    internal interface ITokenService
+    {
+        TokenDto CreateToken(AppUser userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
