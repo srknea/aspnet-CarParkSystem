5186aef4736a1b9e7448e29d43f5456e87e30616
Serkan IŞIK <dehnas79@gmail.com>
2023-09-22 09:37:53 +0300

    Add IAuthenticationService

---
+++ CarParkSystem.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using CarParkSystem.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParkSystem.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<CustomResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<CustomResponseDto<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<CustomResponseDto<NoContentDto>> RevokeRefreshToken(string refreshToken);
+        Task<CustomResponseDto<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
