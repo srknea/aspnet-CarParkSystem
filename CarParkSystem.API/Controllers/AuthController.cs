721b63be73ef932df4a11f994c47569a248eb231
Serkan IŞIK <dehnas79@gmail.com>
2023-09-22 14:35:02 +0300

    Add AuthController

---
+++ CarParkSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using CarParkSystem.Core.DTOs;
+using CarParkSystem.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarParkSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+        private readonly IAuthenticationService _customAuthenticationService;
+
+        public AuthController(IAuthenticationService customAuthenticationService)
+        {
+            _customAuthenticationService = customAuthenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _customAuthenticationService.CreateTokenAsync(loginDto);
+
+            /*
+            if (result.StatusCode == 200)
+            {
+                return Ok();
+            }
+            else if(result.StatusCode == 404)
+            {
+                return NotFound();
+            }
+            */
+
+            return CreateActionResult(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = _customAuthenticationService.CreateTokenByClient(clientLoginDto);
+
+            return CreateActionResult(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _customAuthenticationService.RevokeRefreshToken(refreshTokenDto.RefreshToken);
+
+            return CreateActionResult(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+
+        {
+            var result = await _customAuthenticationService.CreateTokenByRefreshToken(refreshTokenDto.RefreshToken);
+
+            return CreateActionResult(result);
+        }
+    }
+
+}
+}
